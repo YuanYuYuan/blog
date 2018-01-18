d3392617df448b48ab2690d73c1ac84d5a512289
YuanYuYuan <az6980522@gmail.com>
2018-01-18 10:25:04 +0800

    update gem

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "My First Post",
+        "excerpt":"Test!Test2!","categories": ["Diary"],
+        "tags": ["minimal-mistakes","jekyll"],
+        "url": "https://yuanyuyuan.gitlab.io/blog/diary/my-first-post/",
+        "teaser":"https://yuanyuyuan.gitlab.io/blog/assets/images/unsplash-1.jpg"},{
+        "title": "Balancing Car",
+        "excerpt":"This is a project about the self-balancing car.ROBOTSThere are two version of robot car, right one is is 1st ver. robot car in big size, operating in 12V, but since it’s so bulky to balance itself, so there is the 2nd ver. in small size, which is small, compact, modolarized...","categories": ["Robot"],
+        "tags": ["Control","LinkIt 7697","Machine Learning"],
+        "url": "https://yuanyuyuan.gitlab.io/blog/robot/balancing-car/",
+        "teaser":"https://yuanyuyuan.gitlab.io/blog/assets/images/unsplash-1.jpg"},{
+        "title": "Mount Android in Linux",
+        "excerpt":"To communicate with Android Phone in Linux, we have to use MTP, Media Transfer Protocol.InstallationLinux MTP support is provided by libmtp package.For CLI, I recommend use AUR package simple-mtpfs.For GUI, I recommend use Thunar file manager, gvfs virtual filesystem implementation for GIO, gvfs-mtp MTP backend, thunar-volman Thunar volume manager.Usage of...","categories": ["Linux"],
+        "tags": ["Android"],
+        "url": "https://yuanyuyuan.gitlab.io/blog/linux/mount-android-in-linux/",
+        "teaser":"https://yuanyuyuan.gitlab.io/blog/assets/images/unsplash-2.jpg"},{
+        "title": "Using MathJax to display TeX on Jekyll",
+        "excerpt":"Sometimes we may need to some math equations on posts, using LaTeX is the best way but not works on markdown,so we have to import additional tool to deal with it.A recommended way is using MathJax, which is an open-source JavaScript display engine for LaTex, MathML, AsciiMath, and it works...","categories": ["Jekyll"],
+        "tags": ["Jekyll","TeX"],
+        "url": "https://yuanyuyuan.gitlab.io/blog/jekyll/using-mathjax-to-display-tex-on-jekyll/",
+        "teaser":"https://yuanyuyuan.gitlab.io/blog/assets/images/unsplash-3.jpg"},{
+        "title": "Note of Linear Algebra",
+        "excerpt":"Changes2017-10-01: Add invariant subspace","categories": ["math"],
+        "tags": ["Linear Algebra"],
+        "url": "https://yuanyuyuan.gitlab.io/blog/math/linear-algebra-note/",
+        "teaser":"https://yuanyuyuan.gitlab.io/blog/assets/images/unsplash-4.jpg"}]
